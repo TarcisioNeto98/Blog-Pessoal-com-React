451e11a4595bb7bf197f0144d19851232669d07a
TarcisioNeto98 <tarcisioneto98@gmail.com>
2020-07-23 16:47:58 -0500

    Adicionado customização do menu

--- src/App.js
+++ src/App.js
@@ -1,40 +1,65 @@
 import React from 'react';
 import './App.css';
+//import menu from './img/menu.png';
+//import menu from './menu.png';
 
-function lista(props){
-  return <ul></ul>
+function Lista(props){
+  return (
+    <ul className = "">
+      <li id = "negacao">Categoria</li>
+      <li>Projetos</li>
+      <li>Sobre mim</li>
+    </ul>
+  );
+}
+
+class ListaOpcoes extends React.Component{
+  constructor(props){
+    super(props);
+  }
+  render(){
+    return (this.props.clicado) ? <nav className="menu-lateral"><Lista /></nav> : null;
+  }
 }
 
 class BotaoMenu extends React.Component{
   constructor(props){
     super(props);
-  }
-  render(){
-    return <img id = "botao" src="https://github.com/TarcisioNeto98/personal-blog/blob/master/img/menu.png?raw=true" alt="Menu de informações."/>;
-  }
-}
-
-function Menu (props){
-  return(
-    <div>
-      <div className = "menu"><BotaoMenu /></div>
-      <p>asddddd</p>
-      <div className = "">
-        <p>pokemon</p>
-      </div>
-    </div>
-  );
-}
-
-class App extends React.Component {
-
-  constructor(props){
-    super(props);
+    this.clique = this.clique.bind(this);
+    this.state = {clicado: false};
   }
 
+  clique(){
+    this.setState((state) => ({clicado: !state.clicado}));
+    this.props.BotaoClicado(this.state.clicado);
+  }
+//<img id = "botao" src={menu} alt="Menu de informações." onClick={this.clique} />
   render(){
     return (
-      <div className="App"></div>
+      <div id="post">
+        <p id="botao" onClick={this.clique}>&equiv;</p>
+      </div>
+    );
+  }
+}
+
+class Menu extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {clicado: false};
+    this.clicarBotao = this.clicarBotao.bind(this);
+  }
+
+  clicarBotao(clique){
+    this.setState({clicado: clique});
+  }
+
+  render(){
+    return(
+      <div>
+        <div className = {(!this.state.clicado) ? "menu sombra" : "menu2"}><BotaoMenu BotaoClicado = {this.clicarBotao}/></div>
+        <ListaOpcoes clicado = {this.state.clicado}/>
+      </div>
     );
   }
 }
