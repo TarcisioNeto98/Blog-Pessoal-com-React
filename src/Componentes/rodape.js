75fd5e2fb5e015f1457298c7ba9dc5684af2bc62
Tarcisio Neto <tarcisioneto1998@gmail.com>
2020-08-08 17:07:06 -0300

    Adicionado customização do rodape

--- src/Componentes/rodape.js
+++ src/Componentes/rodape.js
@@ -3,11 +3,26 @@ import iconeFacebook from '../img/facebook-icone.png';
 import iconeGithub from '../img/github-icone.png';
 import iconeInstagram from '../img/instagram-icone.png';
 import '../Estilos/rodape.css';
+import telefone from '../img/telefone.svg'
+import localizacao from '../img/gps-icone.svg'
+import email from '../img/email.svg';
 
 class Rodape extends React.Component{
     render(){
         return(
             <div className="rodape">
+
+                <h4>Contato</h4>
+                
+                <img id="email" src={email} alt="Endereço de email"/>
+                <p>tarcisioneto1998@gmail.com</p><br/><br/>
+
+                <img id="localizacao" src={localizacao} alt="Endereço"/>
+                <p>Crateús - CE</p><br/><br/>
+
+                <img id="telefone" src={telefone} alt="Icone de telefone de contato."/>
+                <p>Telefone</p><br/><br/>
+
                 <div className="facebook">
                     <a href="https://www.google.com.br/"><img src={iconeFacebook} alt="Link de ligação com o Facebook."/></a>
                 </div>
