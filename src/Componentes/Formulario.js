eddb7507720d7113d4b3ad81a44e558ac74f266f
Tarcisio Neto <tarcisioneto1998@gmail.com>
2020-08-23 17:08:39 -0300

    Estilização para desktop do formulário adicionada.

--- src/Componentes/Formulario.js
+++ src/Componentes/Formulario.js
@@ -5,14 +5,14 @@ class Formulario extends React.Component{
 
     render(){
         return (
-            <div>
+            <div className="formulario">
                 <h1>Me envie uma mensagem!</h1>
                 <div className="mensagem">
                     <p>Fale comigo, basta preencher os campos devidamente que eu responderei o mais breve possível.</p>
                     <br /><br /><p>Desde já agradeço seu contato!</p><br /><br/>
                 </div>
                 <form>
-                    <input type="text" placeholder="Nome"/><br/>
+                    <input type="text" placeholder="Nome"/>
                     <input type="email" placeholder="Email" /><br/>
                     <textarea placeholder="Digite sua mensagem"></textarea><br/>
                     <input id="botaoEnviar" type="button" value="Enviar" />
