e910caee47fb6c0963148dd5287aefe8b1d91020
Tarcisio Neto <tarcisioneto1998@gmail.com>
2020-08-24 20:08:37 -0300

    Adicionada estilização da página sobre para desktop

--- src/Paginas/Sobre.js
+++ src/Paginas/Sobre.js
@@ -1,25 +1,41 @@
 import React from 'react';
+import cartao from '../img/capa.png';
 import sobreMim from '../img/sobreMim.jpg';
 import '../Estilos/sobre.css';
 
 class Sobre extends React.Component{
-    
     render(){
         return (
             <section className="sobre">
-                <h1>Tarcisio Neto</h1>
-                <h2>Programador javascript e java</h2>
-                <div>
-                    <p>
-                        Com 22 anos, sou graduando em Sistemas de Informação na Universidade Federal do Ceará na cidade
-                        de Crateús, busco sempre aprender novas tecnologias.
-                    </p>
+                <div className="imagem">
+                    <h1>Sobre Mim</h1>
+                    <hr/>
+                    <img id="sobreMim" src={sobreMim} alt="Imagem minha na praia" />
                 </div>
-                <br />
-                <img id="sobreMim" src={sobreMim} alt="Imagem minha na praia" />
+                <div className="cartao">
+                    <img src={cartao} alt="Cartão de perfil"/>
+                    <div className="conteudo">
+                        <br/>
+                        <div id="conteudoTexto">
+                            <p>
+                                Meu nome é Tarcísio, criei esse Blog em 2020 
+                                para compartilhar dicas e informações sobre programação. 
+                                Sempre gostei de programar, já programo em java e javascript. 
+                                O objetivo desse blog é trazer informações e inspirações para que os 
+                                leitores planejem e comecem a adentrar nesse incrível mundo da programação.
+                            </p>
+                        </div>
+                    </div>
+                </div>
             </section>
         );
     }
 }
-
+/*<div>
+<p>
+    Com 22 anos, sou graduando em Sistemas de Informação na Universidade Federal do Ceará na cidade
+    de Crateús, busco sempre aprender novas tecnologias.
+</p>
+</div>
+<br />*/
 export default Sobre;
